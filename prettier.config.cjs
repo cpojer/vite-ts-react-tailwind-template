862c1cf29c30ef8183d115348da5db2f2ebacc7f
cpojer <christoph.pojer@gmail.com>
2023-07-12 09:40:52 +0900

    Update to prettier 3, latest eslint, vite and vitest.

--- prettier.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require.resolve('@ianvs/prettier-plugin-sort-imports'),
-    require.resolve('prettier-plugin-tailwindcss'),
-  ],
-  singleQuote: true,
-  trailingComma: 'all',
-};
