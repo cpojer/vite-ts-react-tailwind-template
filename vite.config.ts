8a2835697d1801c73e26fcfaccae85f6b720edf3
cpojer <christoph.pojer@gmail.com>
2025-04-24 21:23:39 +0900

    Add and enable React Compiler.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import react from '@vitejs/plugin-react';
+import reactCompiler from 'babel-plugin-react-compiler';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [reactCompiler],
+      },
+    }),
+  ],
 });
